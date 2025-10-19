4aa4480ff361b18142e76b747c38dcd4431aeae3
Piker215 <kielbasa7648@gmail.com>
2025-10-19 04:25:04 +0200

    Added an experimental balance

--- expensetracker.cs
+++ expensetracker.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel.Design;
 using System.Formats.Asn1;
@@ -17,12 +18,17 @@ class Program
             {"Keyboard", 50 },
             {"Laptop", 300 },
             {"PC", 600 },
-            {"Mouse Pad", 5 }
+            {"Mouse Pad", 5 },
+            {"Doormat", 20 },
+            {"Lamp", 30 }
         };
         int i = 0;
         int number = 0;
         int sum = 0;
-        a:
+        Console.WriteLine("Welcome to your income tracker");
+            a:
+        Console.WriteLine("Type 'help' to see a list of items of potential buying");
+        Console.WriteLine("Type 'continue' to go to the income phase");
         Console.WriteLine("What products have you bought? (with capital letter start!)");
 
         string action = Console.ReadLine();
@@ -50,14 +56,27 @@ class Program
             goto a;
         }
         else if (products.ContainsKey(action) && i > 0)
-         {
-             Console.WriteLine("How many of these did you buy?");
-             number = Convert.ToInt16(Console.ReadLine());
-             int convert = Convert.ToInt32(products[action]);
-             int result = convert * number;
-             sum += result;
-             Console.WriteLine($"The total cost so far is: {sum}");
-             goto a;
+        {
+            Console.WriteLine("How many of these did you buy?");
+            number = Convert.ToInt16(Console.ReadLine());
+            int convert = Convert.ToInt32(products[action]);
+            int result = convert * number;
+            sum += result;
+            Console.WriteLine($"The total cost so far is: {sum}");
+            goto a;
+        }
+        else if (action == "continue")
+        {
+            Console.WriteLine("Insert the amount of money you'd gained in hand (minus tax) this month:");
+            int income = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Write in all money gifts you've received in this period:");
+            int gifts = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Write in other means of income (i.e. couch sale)");
+            int additional = Convert.ToInt32(Console.ReadLine());
+            int added = income + gifts + additional;
+            int final = added - sum;
+            Console.WriteLine($"Your total this time period comes out to be: {final}");
+            Console.ReadKey();
         }
     }
 }
