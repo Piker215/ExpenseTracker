8bbba8b2b54a4a90785f3fd18dffeed9379486f2
Piker215 <kielbasa7648@gmail.com>
2025-10-23 22:13:47 +0200

    Restructurized entire code

--- expensetracker.cs
+++ expensetracker.cs
@@ -12,15 +12,8 @@ class Program
     static void Main(string[] args)
     {
 
-        Dictionary<string, int> products = new Dictionary<string, int>()
+        Dictionary<string, List<int>> products = new Dictionary<string, List<int>>
         {
-            {"Mouse", 10 },
-            {"Keyboard", 50 },
-            {"Laptop", 300 },
-            {"PC", 600 },
-            {"Mouse Pad", 5 },
-            {"Doormat", 20 },
-            {"Lamp", 30 }
         };
         int i = 0;
         int number = 0;
@@ -30,53 +23,29 @@ class Program
         Console.WriteLine("Type 'help' to see a list of items of potential buying");
         Console.WriteLine("Type 'continue' to go to the income phase");
         Console.WriteLine("What products have you bought? (with capital letter start!)");
-
         string action = Console.ReadLine();
-        if (action == "help")
+        if (action.ToLower() == "help")
         {
-            Console.Clear();
-            foreach (var product in products)
+            foreach (var p in products)
             {
-                Console.WriteLine($"{product.Key}");
+                Console.WriteLine($"{p.Key}: cost = {p.Value[0]}, amount = {p.Value[1]}");
             }
-            Console.WriteLine("Press any key to return");
-            Console.ReadKey();
-            Console.Clear();
             goto a;
         }
-        else if (products.ContainsKey(action) && i == 0)
+        else if (action.ToLower() == "continue")
         {
-            i++;
-            Console.WriteLine("How many of these did you buy?");
-            number = Convert.ToInt16(Console.ReadLine());
-            int convert = Convert.ToInt32(products[action]);
-            int result = convert * number;
-            sum += result;
-            Console.WriteLine($"The cost for this is: {result}");
+            Console.WriteLine("How much do you earn?");     //write here
             goto a;
         }
-        else if (products.ContainsKey(action) && i > 0)
+        else
         {
-            Console.WriteLine("How many of these did you buy?");
-            number = Convert.ToInt16(Console.ReadLine());
-            int convert = Convert.ToInt32(products[action]);
-            int result = convert * number;
-            sum += result;
-            Console.WriteLine($"The total cost so far is: {sum}");
+            Console.WriteLine("How much did you buy?");
+            int amount = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("How much did they cost?");
+            int cost = Convert.ToInt32(Console.ReadLine());
+            products[action] = new List<int> { cost, amount };
+            Console.WriteLine($"Your total comes out to be: {products.Sum(kv => kv.Value[0] * kv.Value[1])} ");
             goto a;
-        }
-        else if (action == "continue")
-        {
-            Console.WriteLine("Insert the amount of money you'd gained in hand (minus tax) this month:");
-            int income = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Write in all money gifts you've received in this period:");
-            int gifts = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Write in other means of income (i.e. couch sale)");
-            int additional = Convert.ToInt32(Console.ReadLine());
-            int added = income + gifts + additional;
-            int final = added - sum;
-            Console.WriteLine($"Your total this time period comes out to be: {final}");
-            Console.ReadKey();
         }
     }
 }
